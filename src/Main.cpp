7be4f2f65f929b1b2de0f9f73de5c8ee3b208c4c
Punit-Koujalgi <punit.utilmore@gmail.com>
2024-11-19 15:48:13 +0530

    decode ints

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cctype>
-#include <cstdlib>
 
-#include "lib/nlohmann/json.hpp"
-
-using json = nlohmann::json;
-
-json decode_bencoded_value(const std::string &encoded_value)
-{
-	if (std::isdigit(encoded_value[0]))
-	{
-		// Example: "5:hello" -> "hello"
-		size_t colon_index = encoded_value.find(':');
-		if (colon_index != std::string::npos)
-		{
-			std::string number_string = encoded_value.substr(0, colon_index);
-			int64_t number = std::atoll(number_string.c_str());
-			std::string str = encoded_value.substr(colon_index + 1, number);
-			return json(str);
-		}
-		else
-		{
-			throw std::runtime_error("Invalid encoded value: " + encoded_value);
-		}
-	}
-	else
-	{
-		throw std::runtime_error("Unhandled encoded value: " + encoded_value);
-	}
-}
+#include "bencode_decoder.h"
 
 int main(int argc, char *argv[])
 {
@@ -55,7 +27,7 @@ int main(int argc, char *argv[])
 		}
 
 		std::string encoded_value = argv[2];
-		json decoded_value = decode_bencoded_value(encoded_value);
+		json decoded_value = Decoder::decode_bencoded_value(encoded_value);
 		std::cout << decoded_value.dump() << std::endl;
 	}
 	else
